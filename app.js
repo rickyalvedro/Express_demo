8d9aa69eaa0602f640dbdbcaa129d81b3698f9e3
Siraj <pintusiraj786@gmail.com>
2022-12-26 14:01:44 +0530

    used body Parser and shown the product on console

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware!");
-  next(); // Allows the request to continue to the next middleware in line
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/", (req, res, next) => {
+  // console.log("This always runs!");
+  next();
 });
 
-app.use((req, res, next) => {
-  console.log("In another middleware!");
+app.use("/add-product", (req, res, next) => {
+  // console.log("In middleware!");
+  res.send(
+    `<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>`
+  );
+});
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  // console.log("In another middleware!");
   res.send("<h1>Hello to Node.js </h1>");
 });
 
