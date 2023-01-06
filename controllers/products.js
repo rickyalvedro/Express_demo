8978053860de5c2d40cadc840cd1275a8a340eea
Siraj <pintusiraj786@gmail.com>
2023-01-06 11:20:37 +0530

    added models

--- controllers/products.js
+++ controllers/products.js
@@ -1,20 +1,23 @@
 const path = require("path");
 
-const express = require("express");
+// const express = require("express");
 
 const rootDir = require("../util/path");
+
+const Product = require("../models/product.js");
 
 exports.getAddProduct = (req, res, next) => {
   res.sendFile(path.join(rootDir, "views", "add-product.html"));
 };
 
 exports.postAddProduct = (req, res, next) => {
-  console.log(req.body);
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  Product.fetchAll((products) => {
+    res.sendFile(path.join(rootDir, "views", "shop.html"));
+  });
 };
-
-
